546a10eeb50757bc5c5d31e941cca76cccb2845a
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-08 14:23:43 +0300

    create SQLAlchemy configuration

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import os
+from dotenv import load_dotenv, find_dotenv
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from .main.main import main_blueprint
 
+db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(main_blueprint)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    db.init_app(app)
 
     return app 
 
 if __name__ == '__main__':
+    load_dotenv (find_dotenv ())
     create_app()
---
+++ main/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from ..app import db
+
+
+class User(db.model):
+    
+ 
+
+
+
