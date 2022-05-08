8b4ac1c806be2875eb720f2b2904cb35ca3c6c06
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-08 12:58:42 +0300

    add dotenv imports

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv (find_dotenv ())
+
+class Config (object):
+    SQLALCHEMY_DATABASE_URI= os.getenv('DATABASE_URL')
+    DEBUG =os.getenv('DEBUG')
+
+    breakpoint()
