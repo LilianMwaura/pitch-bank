7e567604b26ba03c36e6543fdf7eab21cdce7f73
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-09 20:29:53 +0300

    create forms

--- main/models.py
+++ main/models.py
@@ -5,31 +5,47 @@ from ..app import db
 class Users(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String , unique = True)
+    username = db.Column(db.String , unique = True, nullable=False)
     email = db.Column(db.String)
-    def __init__(self,name) -> None:
-       self.name = name
-    
-    def __str__(self) -> str:
-        return self.name
+    password = db.Column(db.String) 
+    pitches = db.relationship('Pitch', backref='owner')
+    comments = db.relationship('Comment', backref='owner')
+    def __repr__(self):
+       return f"User('{self.username}')"
 
-class Categories(db.Model):
-    id= db.Colomn(db.Integer, primary_key = True)
-    name = db.Column(db.String)
+# class Categories(db.Model):
+#     __tablename__ = 'categories'
+
+#     id= db.Column(db.Integer, primary_key = True)
+#     # name = db.Column(db.String)
  
 class Pitches(db.Model):
-    __tablename__ = 'pitches'
-    id= db.Colomn(db.Integer, primary_key = True)
-    category_id = db.Column(db.Integer, db.ForeignKey ["categories.id"])
-    category = db.relationship(Categories) 
-    
+    id= db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey ("user.id"), nullable=False)
+    description = db.Column(db.String(), index=True)
+    title = db.Column(db.String())
+    category = db.Column(db.String(255), nullable=False)  
+    def __repr__(self):
+            return f'Pitch {self.description}'  
+    # category = db.relationship(Categories, backref=db.backref("pitches"), lazy = True)
+
+
 class Comment(db.Model):
-    id= db.Colomn(db.Integer, primary_key = True)
-    comment = db.Column(db.Text)
+    id= db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.String(255), index=True)
+    def __repr__(self):
+            return f'Comment {self.content}'
+
+    # comment = db.Column(db.Text)
+
 class Votes(db.Model):
-      id= db.Colomn(db.Integer, primary_key = True)
+      id= db.Column(db.Integer, primary_key = True)
       up_votes = db.Column(db.Integer)
       down_votes = db.Column(db.Integer)
+      def __repr__(self):
+            return f'Votes{self.votes}'
+
 
 
 
