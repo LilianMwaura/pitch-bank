4bc0549831af4c747ecb2c5b0dd704b7decd3e97
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-09 13:13:33 +0300

    create models

--- main/models.py
+++ main/models.py
@@ -13,5 +13,26 @@ class Users(db.Model):
     def __str__(self) -> str:
         return self.name
 
+class Categories(db.Model):
+    id= db.Colomn(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+ 
+class Pitches(db.Model):
+    __tablename__ = 'pitches'
+    id= db.Colomn(db.Integer, primary_key = True)
+    category_id = db.Column(db.Integer, db.ForeignKey ["categories.id"])
+    category = db.relationship(Categories) 
+    
+class Comment(db.Model):
+    id= db.Colomn(db.Integer, primary_key = True)
+    comment = db.Column(db.Text)
+class Votes(db.Model):
+      id= db.Colomn(db.Integer, primary_key = True)
+      up_votes = db.Column(db.Integer)
+      down_votes = db.Column(db.Integer)
+
+
+
+
 
 
