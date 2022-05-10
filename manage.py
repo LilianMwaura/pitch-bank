12946a9b0f8c084e2cd2610747d19049cde707de
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-10 22:17:34 +0300

    update test.py

---
+++ manage.py
@@ -0,0 +1,17 @@
+from app import create_app,db
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app.models import *
+
+
+# Creating app instance
+FLASK_APP=app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
