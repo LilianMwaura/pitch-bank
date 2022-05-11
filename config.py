5cd27e9d4529450dd9dbdf9748c776337731a932
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-11 12:45:33 +0300

    Deployment

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     DEBUG = True
 
 class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)  
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)  
     ...
 
 class DevConfig(Config):
